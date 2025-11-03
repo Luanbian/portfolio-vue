5950a1d91656d952366ff0ae04e698cf93cbda48
Luan Almeida <luan.almeida@jbtec.com.br>
2025-11-03 18:45:48 -0300

    feat: new habilities

--- src/components/Card-section.vue
+++ src/components/Card-section.vue
@@ -2,21 +2,29 @@
   <All>
     <Space>
       <Card v-for="hability in habilities" :key="hability.id">
-        <BoxIcon v-bind:style="{ backgroundColor: hability.middle ? 'var(--square-color)' : 'var(--light-color)'}">
-          <img :src='hability.icon'/>
+        <BoxIcon
+          v-bind:style="{
+            backgroundColor: hability.middle
+              ? 'var(--square-color)'
+              : 'var(--light-color)',
+          }"
+        >
+          <img :src="hability.icon" />
         </BoxIcon>
         <Subtitle>
           {{ hability.title }}
         </Subtitle>
-        <div style="display: flex;">
-          <img :src='hability.desc.item1'/>
-          <img :src='hability.desc.item2'/>
-          <img :src='hability.desc.item3'/>
-          <img :src='hability.desc.item4'/>
+        <div style="display: flex">
+          <img :src="hability.desc.item1" />
+          <img :src="hability.desc.item2" />
+          <img :src="hability.desc.item3" />
+          <img :src="hability.desc.item4" />
         </div>
-        <div style="display: flex;">
-          <img :src='hability.desc.item5'/>
-          <img :src='hability.desc.item6'/>
+        <div style="display: flex; gap: 10px">
+          <img :src="hability.desc.item5" />
+          <img :src="hability.desc.item6" />
+          <img :src="hability.desc.item7" />
+          <img :src="hability.desc.item8" />
         </div>
       </Card>
     </Space>
@@ -24,56 +32,60 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import '../styles/variables.css'
-import { All, BoxIcon, Card, Space } from '../styles/habilities-styled'
-import { Subtitle } from '../styles/presentation-styled'
+import { defineComponent } from "vue";
+import "../styles/variables.css";
+import { All, BoxIcon, Card, Space } from "../styles/habilities-styled";
+import { Subtitle } from "../styles/presentation-styled";
 
 export default defineComponent({
-  name: 'Card-section',
-  data () {
+  name: "Card-section",
+  data() {
     return {
       habilities: [
         {
           id: 1,
-          icon: '/images/monitor.svg',
-          title: 'Frontend',
+          icon: "/images/monitor.svg",
+          title: "Frontend",
           desc: {
-            item2: '/images/react-icon.svg',
-            item3: '/images/javascript-icon.svg',
-            item4: '/images/typescript-icon.svg',
-            item5: '/images/redux-icon.svg',
-            item6: '/images/nextjs-icon.svg',
-          }
+            item2: "/images/react-icon.svg",
+            item3: "/images/javascript-icon.svg",
+            item4: "/images/typescript-icon.svg",
+            item5: "/images/redux-icon.svg",
+            item6: "/images/nextjs-icon.svg",
+          },
         },
         {
           id: 2,
           middle: true,
-          icon: '/images/database.svg',
-          title: 'Backend',
+          icon: "/images/database.svg",
+          title: "Backend",
           desc: {
-            item1: '/images/typescript-icon.svg',
-            item2: '/images/node-icon.svg',
-            item3: '/images/csharp-icon.svg',
-            item4: '/images/dotnet-icon.svg',
-            item5: '/images/rust-icon.svg',
-          }
+            item1: "/images/typescript-icon.svg",
+            item2: "/images/node-icon.svg",
+            item3: "/images/csharp-icon.svg",
+            item4: "/images/dotnet-icon.svg",
+            item5: "/images/rust-icon.svg",
+            item6: "/images/spring-icon.svg",
+            item7: "/images/java-icon.svg",
+            item8: "/images/kotlin-icon.svg",
+          },
         },
         {
           id: 3,
-          icon: '/images/code.svg',
-          title: 'Extras  ',
+          icon: "/images/code.svg",
+          title: "Extras  ",
           desc: {
-            item1: '/images/jest-icon.svg',
-            item2: '/images/git-icon.svg',
-            item3: '/images/mongo-icon.svg',
-            item4: '/images/graphql-icon.svg',
-            item5: '/images/postgresql-icon.svg'
-          }
-        }
-      ]
-    }
+            item1: "/images/jest-icon.svg",
+            item2: "/images/git-icon.svg",
+            item3: "/images/mongo-icon.svg",
+            item4: "/images/graphql-icon.svg",
+            item5: "/images/postgresql-icon.svg",
+            item6: "/images/docker-icon.svg",
+          },
+        },
+      ],
+    };
   },
-  components: { All, BoxIcon, Card, Subtitle, Space }
-})
+  components: { All, BoxIcon, Card, Subtitle, Space },
+});
 </script>
